c1660d6f3d74b249d434de1605f78fe3d0c0e892
Gizar <gizarir@gmail.com>
2023-02-06 21:44:36 +0200

    Added animation

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     img.setAttribute('src', 'asset/' + imagesArray[0]);
   });
 
+function reset_animation() {
+    var el = document.getElementById('img-center');
+    el.style.animation = 'none';
+    el.offsetHeight; /* trigger reflow */
+    el.style.animation = null; 
+  }
+
 const calcIndex = (i) => {
     const result = (imagesArray.length + n + i) % imagesArray.length;
     if (result === 0) {i = 0};
@@ -17,12 +24,14 @@ function prevBtnOnclick(){
     i--;
     const prev_src = 'asset/' + imagesArray[calcIndex(i)] ;
     img.setAttribute('src', prev_src);
+    reset_animation();
 }
 
 function nextBtnOnClick(){
     i++;
     const next_src = 'asset/' + imagesArray[calcIndex(i)];
     img.setAttribute('src', next_src);
+    reset_animation();
 }
 
 const prevButton = document.getElementById('prev-button');
