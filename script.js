90c9f217f5ab81dfeddfc6e462326e608c26283b
Gizar <gizarir@gmail.com>
2023-02-06 19:15:52 +0200

    First version of task completed

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-const imageArray = ['2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+const imagesArray = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
 let i = 0;
+const n = 100;
+
+const img = document.getElementsByTagName('img')[0];
+document.addEventListener("DOMContentLoaded", function(event) {
+    img.setAttribute('src', 'asset/' + imagesArray[0]);
+  });
+
+const calcIndex = (i) => {
+    const result = (imagesArray.length + n + i) % imagesArray.length;
+    if (result === 0) {i = 0};
+    return result
+};
+
+function prevBtnOnclick(){
+    i--;
+    const prev_src = 'asset/' + imagesArray[calcIndex(i)] ;
+    img.setAttribute('src', prev_src);
+}
+
+function nextBtnOnClick(){
+    i++;
+    const next_src = 'asset/' + imagesArray[calcIndex(i)];
+    img.setAttribute('src', next_src);
+}
+
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button'); 
+
+prevButton.onclick = prevBtnOnclick;
+nextButton.onclick = nextBtnOnClick;
+
+
+
